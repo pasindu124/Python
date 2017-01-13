bd3e024ca077a3821456eb4c62b23d9499fef52a
Pasindu Weerasinghe <pasindurohana@gmail.com>
2017-01-13 12:08:45 +0530

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,31 @@
+class Queue:
+    def __init__(self):
+        self.list=[]
+
+    def enqueue(self,item):
+        self.list.append(item)
+
+    def dequeue(self):
+        self.list.remove(self.list[0])
+
+    def peek(self):
+        return self.list[0]
+
+    def isEmpty(self):
+        return len(self.list)==0
+
+    def printQ(self):
+        return self.list[:]
+
+
+
+#test
+
+q=Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.enqueue(40)
+q.enqueue(50)
+q.dequeue()
+print(q.peek())
